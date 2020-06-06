6367f07812c196f2025753519adc48914e4fb8c2
Alex M <7845120+newAM@users.noreply.github.com>
2020-06-06 12:08:37 -0700

    Increment version for travis to deploy.

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(
     name="hisensetv",
     description="MQTT interface to Hisense televisions.",
     long_description=long_description,
-    version="0.1.0",
+    version="0.1.1",
     author="Alex M.",
     author_email="7845120+newAM@users.noreply.github.com",
     url="https://github.com/newAM/hisensetv",
