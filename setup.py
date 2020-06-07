3606a3f5f7e54f9b280869553880c6c1d0e91663
Alex M <7845120+newAM@users.noreply.github.com>
2020-06-07 12:49:27 -0700

    Fix issue #6.

--- setup.py
+++ setup.py
@@ -6,22 +6,17 @@ this_dir = os.path.abspath(os.path.dirname(__file__))
 with open(os.path.join(this_dir, "README.rst"), "r") as f:
     long_description = f.read()
 
-with open(os.path.join(this_dir, "requirements.txt"), "r") as f:
-    requirements = f.read()
-
-install_requires = [line for line in requirements.splitlines() if line]
-
 setup(
     name="hisensetv",
     description="MQTT interface to Hisense televisions.",
     long_description=long_description,
-    version="0.1.3",
+    version="0.1.4",
     author="Alex M.",
     author_email="7845120+newAM@users.noreply.github.com",
     url="https://github.com/newAM/hisensetv",
     license="MIT",
     python_requires=">=3.6",
-    install_requires=install_requires,
+    install_requires=["paho-mqtt>=1.5.0"],
     classifiers=[
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
